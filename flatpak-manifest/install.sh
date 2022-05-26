3f5a402bf50b835ef2ae54357e252d3c8ff2b880
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2022-05-27 04:48:23 +0530

    flatpak sdk build

--- flatpak-manifest/install.sh
+++ flatpak-manifest/install.sh
@@ -1,1 +1,7 @@
-flatpak-builder --user --install  --force-clean build io.github.WineZGUI.yml
+# find if sdk is installed, else install it
+if [ ! -f ./sdk-installed.txt ]; then
+    flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak list |grep org.freedesktop.Sdk || flatpak --user -y install org.freedesktop.Sdk/x86_64/21.08 && touch ./sdk-installed.txt
+fi
+
+flatpak-builder --user --install  --force-clean build-dir io.github.WineZGUI.yml ||grep -i org.freedesktop.Sdk
