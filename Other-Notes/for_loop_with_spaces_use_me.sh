36c6f52889ede82f5d4b059e7eb7b9f8b319bfd4
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2022-07-12 17:39:47 +0530

    installer support

--- Other-Notes/for_loop_with_spaces_use_me.sh
+++ Other-Notes/for_loop_with_spaces_use_me.sh
@@ -1,16 +1,16 @@
-ln -sf ~/.var/app/io.github.WineZGUI/data/applications/wine/Programs/ ~/.local/share/applications/winezgui/io.github.winezgui-Programs
+ln -sf ~/.var/app/io.github.fastrizwaan.WineZGUI/data/applications/wine/Programs/ ~/.local/share/applications/winezgui/io.github.fastrizwaan.WineZGUI-default
  
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 # do a Sed 
-for i in $(find ~/.var/app/io.github.WineZGUI/data/applications/wine/Programs \
+for i in $(find ~/.var/app/io.github.fastrizwaan.WineZGUI/data/applications/wine/Programs \
            -iname "*.desktop"); do 
 
 echo \"$i\"; 
 sed -E 's/ wine C(.*$)/ wine "C\1"/g' -i "$i"
 sed 's/\\\\ / /g' -i "$i"
 sed 's/\\\\\\\\/\\/g' -i "$i"
-sed 's/wine "C/flatpak run --command=wine io.github.WineZGUI start \/unix "C/g'\
+sed 's/wine "C/flatpak run --command=wine io.github.fastrizwaan.WineZGUI start \/unix "C/g'\
     -i "$i"
 done
        
@@ -19,6 +19,6 @@ IFS=$SAVEIFS
 
 
 #####
-#cat "/home/rizvan/.var/app/io.github.WineZGUI/data/applications/wine/Programs/Angel Writer/Angel Writer.desktop"|sed -E 's/ wine C(.*$)/ wine "C\1"/g'|sed 's/\\\\ / /g'|sed 's/\\\\\\\\/\\/g'
+#cat "/home/rizvan/.var/app/io.github.fastrizwaan.WineZGUI/data/applications/wine/Programs/Angel Writer/Angel Writer.desktop"|sed -E 's/ wine C(.*$)/ wine "C\1"/g'|sed 's/\\\\ / /g'|sed 's/\\\\\\\\/\\/g'
 
 
